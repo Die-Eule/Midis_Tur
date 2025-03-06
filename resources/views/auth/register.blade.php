38a49539da179db7f24d1d311369877dbe4f2b4c
kilina irina p42 <gisi@bk.ru>
2025-03-06 19:05:57 +0500

    КилинаИВ_В регистрации: Перевела поля имени, пароля и подтверждения пароля, а также кнопку регистрации и ссылку для перехода на страницу входа. Также добавила поля фамилии, отчества и телефона. Во входе: Перевела поле пароля и кнопку входа. Также изменила ссылку "забыли пароль?", на "Ещё не зарегистрированы?" и сделала ей переход на страницу регистрации. На обоих страницах добавила свои стили.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,23 +2,44 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
+        <!-- Surname -->
+        <div class="mt-4">
+            <x-input-label for="surname" :value="__('Фамилия')" />
+            <x-text-input id="surname" class="block mt-1 w-full" type="text" name="surname" :value="old('surname')" required autofocus autocomplete="family-name" />
+            <x-input-error :messages="$errors->get('surname')" class="mt-2" />
+        </div>
+        
         <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+        <div class="mt-4">
+            <x-input-label for="name" :value="__('Имя')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="given-name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
+        <!-- Patranomic -->
+        <div class="mt-4">
+            <x-input-label for="patranomic" :value="__('Отчество')" />
+            <x-text-input id="patranomic" class="block mt-1 w-full" type="text" name="patranomic" :value="old('patranomic')" required autofocus autocomplete="additional-name" />
+            <x-input-error :messages="$errors->get('patranomic')" class="mt-2" />
+        </div>
+        
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('E-mail')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <!-- Phone -->
+        <div class="mt-4">
+            <x-input-label for="tel" :value="__('Телефон')" />
+            <x-text-input id="tel" class="block mt-1 w-full" type="tel" name="tel" :value="old('tel')" required autofocus autocomplete="additional-tel" />
+            <x-input-error :messages="$errors->get('tel')" class="mt-2" />
+        </div>
+
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Пароль')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -30,7 +51,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Подтверждение пароля')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -39,13 +60,13 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+        <div class="flex items-end justify-end mt-4">
+            <a class="mb-2 font-normal underline text-sm text-black-700 dark:text-black-700 hover:text-orange-600 dark:hover:text-gray-100 rounded-md" href="{{ route('login') }}">
+                {{ __('Уже зарегистрированы?') }}
             </a>
 
             <x-primary-button class="ms-4">
-                {{ __('Register') }}
+                {{ __('Регистрация') }}
             </x-primary-button>
         </div>
     </form>
