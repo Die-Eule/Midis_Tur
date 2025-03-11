0366943f07bddb73d326f67beaad867c8f483ee3
kilina irina p42 <gisi@bk.ru>
2025-03-12 00:30:03 +0500

    КилинаИВ_Сделала заготовки страниц (dashboard).

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,10 @@
 <x-app-layout>
-    
+    <x-slot name="header1"></x-slot>
+
+    <!--Body-->
+    <div class="w-full h-[1080px]">
+        <a class="text-9xl text-center text-stone-50" href="{{ route('dashboard2') }}" class="flex items-center">
+            <p>Ссылка</p>
+        </a>
+    </div>
 </x-app-layout>
---
+++ resources/views/dashboard2.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-slot name="header2"></x-slot>
+
+    <!--Body-->
+    <div class="w-full h-[1080px]">
+        <a class="text-9xl text-center" href="{{ route('dashboard') }}" class="flex items-center">
+            <p>Ссылка</p>
+        </a>
+    </div>
+</x-app-layout>
