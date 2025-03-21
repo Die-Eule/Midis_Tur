008219c89d158b27f4e234ec8309a6b739cd6164
kilina irina p42 <gisi@bk.ru>
2025-03-21 21:46:13 +0500

    КилинаИВ_Добавила тени карточкам.

--- resources/views/dashboard2.blade.php
+++ resources/views/dashboard2.blade.php
@@ -6,17 +6,16 @@
         <!-- <a class="text-9xl text-center" href="{{ route('dashboard') }}" class="flex items-center">
             <p>Ссылка</p>
         </a> -->
-        <!--Поменять цвет фона на такой же как на входе и регистрации-->
-        <div class="font-kslab uppercase mt-10 flex flex-col justify-center items-center text-3xl text-orange-600">
+        <div class="font-kslab uppercase mt-16 flex flex-col justify-center items-center text-3xl text-orange-600">
             <p>Где вы планируете проходить обучение?</p>
             <p class="mt-8">Колледж на базе 9 классов / 11 классов</p>
         </div>
 
         <div class="xl:columns-2 mt-14">
-            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6">
+            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6 shadow-lg">
                 <p class="font-kslab text-orange-600 text-lg mb-4">Дизайн</p>
                 <ul role="list" class="list-disc marker:text-black ml-6 text-base/7">
-                    <li>Графический дизайн и брендинг</li>
+                    <li><a href="{{ route('dashboard3') }}" class="flex items-center">Графический дизайн и брендинг</a></li>
                     <li>3D Моделирование для компьютерных игр</li>
                     <li>Дизайн одежды и аксессуаров</li>
                     <li>Дизайн интерьера</li>
@@ -24,14 +23,14 @@
                 </ul>
             </div>
 
-            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6">
+            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6 shadow-lg">
                 <p class="font-kslab text-orange-600 text-lg mb-4">IT-технологии</p>
                 <ul role="list" class="list-disc marker:text-black  ml-6">
                     <li>Разработка веб и мультимедийных приложений</li>
                 </ul>
             </div>
 
-            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6">
+            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6 shadow-lg">
                 <p class="font-kslab text-orange-600 text-lg mb-4">Экономика и управление</p>
                 <ul role="list" class="list-disc marker:text-black  ml-6">
                     <li>Предпринимательство и интернет-маркетинг</li>
@@ -39,7 +38,7 @@
                 </ul>
             </div>
 
-            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6">
+            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6 shadow-lg">
                 <p class="font-kslab text-orange-600 text-lg mb-4">Туризм и гостеприимство</p>
                 <ul role="list" class="list-disc marker:text-black  ml-6">
                     <li>Администрирование отеля и организация экскурсионных услуг</li>
@@ -52,7 +51,7 @@
         </div>
 <!-- start-changin -->
         <div class="xl:columns-2 mt-14">
-            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6">
+            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6 shadow-lg">
                 <p class="font-kslab text-orange-600 text-lg mb-4">Дизайн</p>
                 <ul role="list" class="list-disc marker:text-black ml-6 text-base/7">
                     <li>Цифровая графика в индустрии компьютерных игр</li>
@@ -65,7 +64,7 @@
                 </ul>
             </div>
 
-            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6">
+            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6 shadow-lg">
                 <p class="font-kslab text-orange-600 text-lg mb-4">Ресторанный и гостиничный бизнес</p>
                 <ul role="list" class="list-disc marker:text-black  ml-6">
                     <li>Менеджмент в ресторанном и гостиничном бизнесе</li>
@@ -74,7 +73,7 @@
                 </ul>
             </div>
 
-            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6">
+            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6 shadow-lg">
                 <p class="font-kslab text-orange-600 text-lg mb-4">IT - технологии</p>
                 <ul role="list" class="list-disc marker:text-black  ml-6">
                     <li>Разработка компьютерных игр и приложений с виртуальной и дополненной реальностью (VR/AR)</li>
@@ -83,14 +82,14 @@
                 </ul>
             </div>
 
-            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6">
+            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6 shadow-lg">
                 <p class="font-kslab text-orange-600 text-lg mb-4">Лингвистика</p>
                 <ul role="list" class="list-disc marker:text-black  ml-6">
                     <li>Перевод и международные бизнес-коммуникации</li>
                 </ul>
             </div>
 
-            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6">
+            <div class="bg-white p-10 rounded-xl w-[500px] mb-10 mx-6 shadow-lg">
                 <p class="font-kslab text-orange-600 text-lg mb-4">Экономика и управление</p>
                 <ul role="list" class="list-disc marker:text-black  ml-6">
                     <li>Управление бизнесом и интернет-маркетинг</li>
@@ -99,5 +98,28 @@
             </div>
         </div>
         
+    </div>
+
+    <!--footer-->
+    <div class="max-w-full p-10 mt-16 sm:px-6 lg:px-8 flex justify-center items-center max-h-[64px] text-orange-600">
+
+        <!-- Logo -->
+        <div class="shrink-0 flex items-center min-w-[20rem]">
+            <a href="{{ route('dashboard') }}" class="flex items-center">
+                <img src="{{ Vite::asset('resources/images/logo.png') }}" alt="logo" class="w-10">
+                <p class="w-64 ml-6 font-medium">Международный институт дизайна и сервиса</p>
+            </a>
+        </div>
+
+        <div class="flex items-center min-w-[20rem] lg:mx-20">
+            <img src="{{ Vite::asset('resources/images/telegram_orange.png') }}" alt="telegram-icon" class="w-10">
+            <img src="{{ Vite::asset('resources/images/vk_orange.png') }}" alt="vk-icon" class="w-10">
+            <div class="ml-8 font-medium">
+                <p>+7 (351) 202-00-73</p>
+                <p>info@midis.ru</p>
+            </div>
+        </div>
+
+        <p>© 2025</p>
     </div>
 </x-app-layout>
