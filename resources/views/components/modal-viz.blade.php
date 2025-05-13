46134e2dec07fbd357b7af7bff971493af75d5bc
kilina irina p42 <gisi@bk.ru>
2025-05-13 23:13:17 +0500

    КилинаИВ_Создала компонет modal-viz как копию modal для отображения галлереи фото и видео. Откатила все изменения сделанные до этого в оригинальном компоненте modal

--- resources/views/components/modal-viz.blade.php
+++ resources/views/components/modal-viz.blade.php
@@ -1,7 +1,9 @@
 @props([
     'name',
     'show' => false,
-    'maxWidth' => '2xl'
+    'maxWidth' => '2xl',
+    'shift' => false,
+    'bg' => true
 ])
 
 @php
@@ -11,7 +13,11 @@ $maxWidth = [
     'lg' => 'sm:max-w-lg',
     'xl' => 'sm:max-w-xl',
     '2xl' => 'sm:max-w-2xl',
+    '7xl' => 'sm:max-w-7xl',
 ][$maxWidth];
+
+$shift = $shift ? 'sm:mt-20' : '';
+$background = $bg ? 'bg-black/70' : '';
 @endphp
 
 <div
@@ -35,8 +41,17 @@ $maxWidth = [
         if (value) {
             document.body.classList.add('overflow-y-hidden');
             {{ $attributes->has('focusable') ? 'setTimeout(() => firstFocusable().focus(), 100)' : '' }}
+            $videoPlayer = $el.querySelector('video');
+            if ($videoPlayer) {
+                $videoPlayer.play();
+            }
         } else {
             document.body.classList.remove('overflow-y-hidden');
+            $videoPlayer = $el.querySelector('video');
+            if ($videoPlayer) {
+                $videoPlayer.pause();
+                $videoPlayer.currentTime = 0;
+            }
         }
     })"
     x-on:open-modal.window="$event.detail == '{{ $name }}' ? show = true : null"
@@ -65,7 +80,7 @@ $maxWidth = [
 
     <div
         x-show="show"
-        class="mb-6 bg-white dark:bg-gray-800 rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
+        class="mb-6 {{ $background }} rounded-lg overflow-hidden transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto {{ $shift }}"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
@@ -74,5 +89,5 @@ $maxWidth = [
         x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
     >
         {{ $slot }}
-    </div>
+    </div>  
 </div>
