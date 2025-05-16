ba7eb877603f5d5ec5a7718f1b8a9c2a30f8df02
kilina irina p42 <gisi@bk.ru>
2025-05-17 04:21:06 +0500

    КилинаИВ_Добавила метод update в контроллер направлений обучения, который выполняет комплексую обработку данных из формы редактирования карточки направления обучения: обновляет данные по существующим ключам, удаляет записи, ключи которых не были переданы, добавляет новые записи.

--- app/Http/Controllers/DirectionController.php
+++ app/Http/Controllers/DirectionController.php
@@ -13,4 +13,46 @@ class DirectionController extends Controller
         $specialties = Specialty::all()->groupBy('direction_id');
         return view('dashboard2', compact('directions', 'specialties'));
     }
+
+    public function update(Request $req)
+    {
+        $input = $req->collect();
+        $direction = Direction::where('id', $input['direction'])->first();
+        $specialties = Specialty::where('direction_id', $input['direction'])->get();
+        $upd_specs = $input->filter(
+            function($item, $key) {
+                return is_numeric($key) && $item;
+            }
+        );
+
+        $new_specs = $input->filter(
+            function($item, $key) {
+                return substr($key, 0, 4) === "new_" && $item;
+            }
+        )->values();
+
+        $direction->name = substr($input['title'], 0, 255);
+        $direction->save();
+
+        $specialties->each(
+            function($item, $key) use($upd_specs){
+                if ($upd_specs->has($item->id)) {
+                    $item->name = substr($upd_specs[$item->id], 0, 255);
+                    $item->save();
+                } else {
+                    $item->delete();
+                }
+            }
+        );
+
+        $new_specs->each(
+            function($item, $key) use($direction) {
+                $data['name'] = substr($item, 0, 255);
+                $data['direction_id'] = $direction->id;
+                Specialty::create($data);
+            }
+        );
+
+        return redirect()->back();
+    }
 }
--- app/Models/Specialty.php
+++ app/Models/Specialty.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Specialty extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'direction_id',
+    ];
 }
