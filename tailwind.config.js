3468743734f1028faf09667b18f62b492809642b
kilina irina p42 <gisi@bk.ru>
2025-06-01 00:28:11 +0500

    КилинаИВ_Обновила Alpine.js до последней версии.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
+        './app/Models/*.php',
         './resources/views/**/*.blade.php',
         './node_modules/flowbite/**/*.js'
     ],
