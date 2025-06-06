43cc1ad623b0f05d48ceb8b4f05fccc69e74fe46
kilina irina p42 <gisi@bk.ru>
2025-06-07 03:44:30 +0500

    КилинаИВ_Масштабирование представления Галлерея до 340px. Отключила просмотр через модал для мобильных устройств.

--- resources/views/dashboard6.blade.php
+++ resources/views/dashboard6.blade.php
@@ -3,21 +3,24 @@
     <!--Body-->
     <div class="w-full h-full flex flex-col items-center" x-data="{ collection: [], selected: '' }">
         <div class="font-kslab uppercase mt-16 flex justify-center items-center text-3xl text-orange-600">
-            <div class="w-[32px] h-[32px]">
-                <a href="{{ route('dashboard3', $department->id) }}" class="pr-8">
-                    <img src="{{ Vite::asset('resources/images/arrow.svg') }}" class="hover:h-[32px]" alt="">
-                </a>
+            <div class="flex flex-col md:flex-row items-center">
+                <div>
+                    <a href="{{ route('dashboard3', $department->id) }}" class="flex items-center mb-8 md:mb-0">
+                        <div class="w-[32px] h-[32px]"><img src="{{ Vite::asset('resources/images/arrow.svg') }}" class="hover:h-[32px]" alt=""></div>
+                        <p class="md:hidden text-base pl-2">Назад</p>
+                    </a>
+                </div>
+                <p class="px-5 md:px-0 text-center">{{$department->name}}</p>
             </div>
-            <p>{{$department->name}}</p>
         </div>
 
         <div class="grid grid-cols-1 xl:grid-cols-2 gap-4 mt-14 select-none">
-            @ifadmin(Auth::user())
+            @ifadmin
                 <form enctype="multipart/form-data" method="post" action="{{ route('dashboard6.upload', $department->id) }}"
                         >
                     @csrf
                     @method('put')
-                    <div class="w-[500px] h-[320px] shadow-lg rounded-xl bg-white mb-10 mx-6 flex justify-center items-center cursor-pointer"
+                    <div class="w-[89vw] h-[57vw] ns:w-[500px] ns:h-[320px] shadow-lg rounded-xl bg-white mb-10 mx-6 flex justify-center items-center cursor-pointer"
                             x-on:click.prevent.stop="$refs.browse.click()">
                         <img src="{{ Vite::asset('resources/images/plus_sign.svg') }}" alt="">
                     </div>
@@ -27,22 +30,22 @@
                 </form> 
             @endifadmin
             @foreach($gallery as $item)
-                <div class="relative w-[500px] h-[320px] mb-10 mx-6 cursor-pointer">
-                    @ifadmin(Auth::user())
-                        <div class="absolute w-[500px] h-[320px] flex justify-center items-center bg-black/50 opacity-0 hover:opacity-100 duration-500 rounded-xl"
+                <div class="relative w-[89vw] h-[57vw] ns:w-[500px] ns:h-[320px] mb-10 mx-6 cursor-pointer">
+                    @ifadmin
+                        <div class="absolute w-[89vw] h-[57vw] ns:w-[500px] ns:h-[320px] flex justify-center items-center bg-black/50 opacity-0 hover:opacity-100 duration-500 rounded-xl"
                             id="{{$item->id}}" x-on:click.prevent.stop="selected=$el.id; $dispatch('open-modal', 'del-photo')">
                             <p class="text-5xl font-medium text-orange-500">&#128465;</p>
                         </div>
                     @endifadmin
                     <img src="{{ $item->url }}" alt="" id="{{$loop->index}}"
-                        class="w-[500px] h-[320px] rounded-xl object-cover"
+                        class="w-[89vw] h-[57vw] ns:w-[500px] ns:h-[320px] rounded-xl object-cover"
                         x-init="collection[{{$loop->index}}]=$el.src"
-                        x-on:click.prevent.stop="selected=$el.id; $dispatch('open-modal', 'gallery')">
+                        x-on:click.prevent.stop="selected=$el.id; if(window.matchMedia('(pointer: fine)').matches) $dispatch('open-modal', 'gallery');">
                 </div>
             @endforeach
         </div>
 
-        @ifadmin(Auth::user())
+        @ifadmin
 
             <x-modal name="del-photo" maxWidth="lg">
                 <form method="post" action="{{ route('dashboard6.remove') }}"
@@ -67,7 +70,7 @@
                     <div class="w-[20px] cursor-pointer" x-on:click="selected > 0 ? selected-- : null">
                         <img src="{{ Vite::asset('resources/images/arrow.svg') }}" class="hover:w-[20px]" alt="">
                     </div>
-                    <img :src="collection[selected]" alt="" class="mx-5 sm:max-w-6xl max-h-[94vh]">
+                    <img :src="collection[selected]" alt="" class="mx-5 max-w-xl md:max-w-2xl xl:max-w-6xl max-h-[94vh]">
                     <div class="w-[20px] cursor-pointer" x-on:click="selected < {{ $gallery->count() - 1 }} ? selected++ : null">
                         <img src="{{ Vite::asset('resources/images/arrow.svg') }}" class="hover:w-[20px] -scale-x-100" alt="">
                     </div>
